cfc2662a2532e71d95383e29f84f00e6c7f6735d
Mark <mark@msnyder.com>
2023-01-10 16:10:24 -0500

    fixed html and added ami lamp install

---
+++ aws-ami/index.php
@@ -0,0 +1,11 @@
+<html>
+     <head>
+         <title>Welcome</title>
+     </head>
+ <body style="color: #ffffff; background-color: #000000" >
+     <p>  This is host $(hostname) </p> 
+     <p><img src="vault.png"></p>
+    <p><?php echo "hostname is:".gethostname(); ?></p>
+ </body>
+</html>
+
