31bad5edd52aafcd9ce3a64156cf4f9152d3a50a
Mark <mark@msnyder.com>
2023-01-13 15:14:25 -0500

    typo

--- aws-ami/index.php
+++ aws-ami/index.php
@@ -3,7 +3,6 @@
          <title>Welcome</title>
      </head>
  <body style="color: #ffffff; background-color: #000000" >
-     <p>  This is host $(hostname) </p> 
      <p><img src="vault.png"></p>
     <p><?php echo "hostname is:".gethostname(); ?></p>
  </body>
