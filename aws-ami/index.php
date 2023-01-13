86c54ee9a8ed0b2ecdc6170d95da3b7a390a72d1
Mark <mark@msnyder.com>
2023-01-13 17:01:38 -0500

    img

--- aws-ami/index.php
+++ aws-ami/index.php
@@ -3,7 +3,7 @@
          <title>Welcome</title>
      </head>
  <body style="color: #ffffff; background-color: #000000" >
-     <p><img src="vault.png"></p>
+     <p><img src="vault.png width="300" height="300"></p>
     <p><?php echo "hostname is:".gethostname(); ?></p>
     <video width="640" height="360" controls muted loop autoplay>
     <source src="Countdown.mp4" type="video/mp4">
