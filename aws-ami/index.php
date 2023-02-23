81d3be18a0f9829f7aa8c587885a245b39fb6c0a
Mark <mark@msnyder.com>
2023-02-22 20:09:21 -0500

    add get source user IP

--- aws-ami/index.php
+++ aws-ami/index.php
@@ -4,7 +4,8 @@
      </head>
  <body style="color: #ffffff; background-color: #000000" >
      <p><img src="vault.png" width="500" height="300"></p>
-    <p><?php echo "hostname is:".gethostname(); ?></p>
+    <p><?php echo "Destination host:".gethostname(); ?></p>
+    <p><?php $ipaddress = getenv("REMOTE_ADDR") ; Echo "Source IP Address" . $ipaddress; ?></p>
     <video width="640" height="360" controls muted loop autoplay>
     <source src="Countdown.mp4" type="video/mp4">
     </video>
